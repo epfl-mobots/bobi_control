95b95baa987671a8231312de0443eb710478f651
Vaios Papaspyros <b.papaspyros@gmail.com>
2021-05-06 12:57:14 +0200

    Add circular trajectory example with pos control + launch files

---
+++ src/test/circular_trajectory_node.cpp
@@ -0,0 +1,39 @@
+#include <ros/ros.h>
+#include <bobi_msgs/PoseStamped.h>
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "circular_trajectory_node");
+    std::shared_ptr<ros::NodeHandle> nh(new ros::NodeHandle());
+
+    float radius;
+    float waypoint_angle;
+    nh->param<float>("circular_trajectory/radius", radius, 0.25);
+    nh->param<float>("circular_trajectory/waypoint_angle", waypoint_angle, 0.26);
+
+    ros::Publisher wp_pub = nh->advertise<bobi_msgs::PoseStamped>("target_position", 1);
+
+    int num_waypoints = static_cast<int>(2. * M_PI / waypoint_angle);
+    std::vector<bobi_msgs::PoseStamped> waypoints;
+    for (int i = 0; i < num_waypoints; ++i) {
+        bobi_msgs::PoseStamped wp;
+        wp.pose.xyz.x = radius * std::cos(waypoint_angle * i);
+        wp.pose.xyz.y = radius * std::sin(waypoint_angle * i);
+        waypoints.push_back(wp);
+    }
+
+    ros::Rate loop_rate(1);
+    int iter = 0;
+    while (ros::ok()) {
+        wp_pub.publish(waypoints[iter++]);
+        std::cout << waypoints[iter++] << std::endl;
+        if (iter == num_waypoints) {
+            iter = 0;
+        }
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+    return 0;
+}
