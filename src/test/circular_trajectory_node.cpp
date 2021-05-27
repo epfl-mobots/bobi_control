12b0ee4da540e861077706b43ad26f017d7cb097
Vaios Papaspyros <b.papaspyros@gmail.com>
2021-05-27 17:12:02 +0200

    Rename yaml file

--- src/test/circular_trajectory_node.cpp
+++ src/test/circular_trajectory_node.cpp
@@ -7,8 +7,10 @@ int main(int argc, char** argv)
     std::shared_ptr<ros::NodeHandle> nh(new ros::NodeHandle());
 
     float radius;
+    std::vector<float> center;
     float waypoint_angle;
     nh->param<float>("circular_trajectory/radius", radius, 0.25);
+    nh->param<std::vector<float>>("circular_trajectory/center", center, {0., 0.});
     nh->param<float>("circular_trajectory/waypoint_angle", waypoint_angle, 0.26);
 
     ros::Publisher wp_pub = nh->advertise<bobi_msgs::PoseStamped>("target_position", 1);
@@ -17,8 +19,8 @@ int main(int argc, char** argv)
     std::vector<bobi_msgs::PoseStamped> waypoints;
     for (int i = 0; i < num_waypoints; ++i) {
         bobi_msgs::PoseStamped wp;
-        wp.pose.xyz.x = radius * std::cos(waypoint_angle * i);
-        wp.pose.xyz.y = radius * std::sin(waypoint_angle * i);
+        wp.pose.xyz.x = radius * std::cos(waypoint_angle * i) + center[0];
+        wp.pose.xyz.y = radius * std::sin(waypoint_angle * i) + center[1];
         waypoints.push_back(wp);
     }
 
