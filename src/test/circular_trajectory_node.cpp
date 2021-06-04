ce6f7381e373465f2ac1b0e1b4287ce7f2aee07a
Vaios Papaspyros <b.papaspyros@gmail.com>
2021-06-04 15:25:58 +0200

    Rate and direction are set from yaml for circular test

--- src/test/circular_trajectory_node.cpp
+++ src/test/circular_trajectory_node.cpp
@@ -9,22 +9,31 @@ int main(int argc, char** argv)
     float radius;
     std::vector<float> center;
     float waypoint_angle;
+    int rate;
+    bool cw;
     nh->param<float>("circular_trajectory/radius", radius, 0.25);
     nh->param<std::vector<float>>("circular_trajectory/center", center, {0., 0.});
     nh->param<float>("circular_trajectory/waypoint_angle", waypoint_angle, 0.26);
+    nh->param<int>("circular_trajectory/rate", rate, 2);
+    nh->param<bool>("circular_trajectory/cw", cw, true);
 
     ros::Publisher wp_pub = nh->advertise<bobi_msgs::PoseStamped>("target_position", 1);
+
+    int sign = 1;
+    if (!cw) {
+        sign = -1;
+    }
 
     int num_waypoints = static_cast<int>(2. * M_PI / waypoint_angle);
     std::vector<bobi_msgs::PoseStamped> waypoints;
     for (int i = 0; i < num_waypoints; ++i) {
         bobi_msgs::PoseStamped wp;
-        wp.pose.xyz.x = radius * std::cos(waypoint_angle * i) + center[0];
-        wp.pose.xyz.y = radius * std::sin(waypoint_angle * i) + center[1];
+        wp.pose.xyz.x = radius * std::cos(sign * waypoint_angle * i) + center[0];
+        wp.pose.xyz.y = radius * std::sin(sign * waypoint_angle * i) + center[1];
         waypoints.push_back(wp);
     }
 
-    ros::Rate loop_rate(2);
+    ros::Rate loop_rate(rate);
     int iter = 0;
     while (ros::ok()) {
         ROS_INFO("(x=%f, y=%f)", waypoints[iter].pose.xyz.x, waypoints[iter].pose.xyz.y);
